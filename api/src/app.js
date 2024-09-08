38de9d1563fb0788c2fb712df99d94246a608c07
esteban900 <estebanandrusyszyn90@gmail.com>
2024-09-08 18:27:47 -0300

    cambios env render add

--- api/src/app.js
+++ api/src/app.js
@@ -13,6 +13,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require ('body-parser');
 const morgan = require('morgan');
 const mainRouter = require ('./routes/index');
+const cors = require('cors');
 
 require('./db');
 
@@ -27,13 +28,19 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+//   });
+app.use(cors({
+  origin: 'https://your-frontend-url.vercel.app', // Cambia esto a la URL de tu frontend
+  credentials: true,
+  methods: 'GET,POST,OPTIONS,PUT,DELETE',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
+}));
   
   app.use(mainRouter);
   
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export const GET_PRODUCT_SUBCATEGORIAS_ALL = "GET_PRODUCT_SUBCATEGORIAS_ALL";
 export const POST_CREATE_PRODUCT = 'POST_CREATE_PRODUCT';
@@ -7,7 +8,7 @@ export const POST_CREATE_PRODUCT = 'POST_CREATE_PRODUCT';
 export const getProductsSubCategorias = (tipo_venta, categoria, subcategoria) => {
     return async function (dispatch) {
         try {
-            const response = await axios.get(`http://localhost:3001/menu/${tipo_venta}/${categoria}/${subcategoria}`);
+            const response = await axios.get(`${apiUrl}/menu/${tipo_venta}/${categoria}/${subcategoria}`);
             const productsSubCategorias = response.data;
 
             dispatch({
@@ -33,7 +34,7 @@ export const postProduct = (formData) => {
                 // console.log(pair[0] + ', ' + pair[1]);
             }
 
-            const response = await axios.post("http://localhost:3001/product", formData, {
+            const response = await axios.post(`${apiUrl}/product`, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
