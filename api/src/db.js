c3756d2ce5c4a9e04c75b3c08822c6ea2e17d86d
esteban900 <estebanandrusyszyn90@gmail.com>
2024-09-08 17:35:42 -0300

    Cambio en db seq url

--- api/src/db.js
+++ api/src/db.js
@@ -5,10 +5,15 @@ const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/menupolonia`, {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  });
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/menupolonia`, {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+  native: false,
+});
   const basename = path.basename(__filename);
   
   const modelDefiners = [];
