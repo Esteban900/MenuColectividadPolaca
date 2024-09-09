16e2634566d027820f53a032fcf5f66c4efbae29
esteban900 <estebanandrusyszyn90@gmail.com>
2024-09-09 19:08:37 -0300

    cards3

--- client/src/Components/Cards/Cards.jsx
+++ client/src/Components/Cards/Cards.jsx
@@ -29,20 +29,3 @@ const Card = (props) => {
 
 export default Card;
 
-// import style from './Cards.module.css';
-
-// const Card = (props) => {
-//     return(
-//         <div className={style.Card}>
-//             <img src={props.imageUrl} alt='img not found' className={style.image}/>
-//             <div className={style.content}>
-//         <h2 className={style.title}>{props.name}</h2>
-//         <span className={style.price}>$ {props.cost}</span>
-//         <p className={style.description}>{props.description}</p>
-//       </div>
-//       </div>
-      
-//     )
-// };
-
-// export default Card;
