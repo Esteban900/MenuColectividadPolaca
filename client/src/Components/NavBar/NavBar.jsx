2487d1f5fb61f1738fe6a3c9437ca6ed3a3ad8bd
esteban900 <estebanandrusyszyn90@gmail.com>
2024-09-11 18:33:42 -0300

    sin cache

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import style from './NavBar.module.css';
 import { getProductsSubCategorias } from "../../redux/actions";
@@ -13,29 +13,34 @@ const NavBar = ({ defaultSelectedMenu, defaultSelectedOption }) => {
     const [selectedPlatoOption, setSelectedPlatoOption] = useState('');
     const [selectedBebidaOption, setSelectedBebidaOption] = useState('');
 
-    const cache = useSelector((state) => state.cache);
-
+    // useEffect(() => {
+    //     if (defaultSelectedMenu === 'Platos Tipicos') {
+    //         setSelectedMenu('Platos Tipicos');
+    //         setSelectedPlatoOption(defaultSelectedOption);
+    //         setSelectedBebidaOption('');
+    //         dispatch(getProductsSubCategorias('Salon', 'Platos Tipicos', defaultSelectedOption));
+    //     } else if (defaultSelectedMenu === 'Bebidas') {
+    //         setSelectedMenu('Bebidas');
+    //         setSelectedPlatoOption('');
+    //         setSelectedBebidaOption(defaultSelectedOption);
+    //         dispatch(getProductsSubCategorias('Salon', 'Bebidas', defaultSelectedOption));
+    //     }
+    // }, [defaultSelectedMenu, defaultSelectedOption, dispatch]);
     useEffect(() => {
         if (defaultSelectedMenu && defaultSelectedOption) {
             if (defaultSelectedMenu === 'Platos Tipicos') {
                 setSelectedMenu('Platos Tipicos');
                 setSelectedPlatoOption(defaultSelectedOption);
                 setSelectedBebidaOption('');
-                const cacheKey = `Salon_Platos Tipicos_${defaultSelectedOption}`;
-                if (!cache[cacheKey]) {
-                    dispatch(getProductsSubCategorias('Salon', 'Platos Tipicos', defaultSelectedOption));
-                }
+                dispatch(getProductsSubCategorias('Salon', 'Platos Tipicos', defaultSelectedOption));
             } else if (defaultSelectedMenu === 'Bebidas') {
                 setSelectedMenu('Bebidas');
                 setSelectedPlatoOption('');
                 setSelectedBebidaOption(defaultSelectedOption);
-                const cacheKey = `Salon_Bebidas_${defaultSelectedOption}`;
-                if (!cache[cacheKey]) {
-                    dispatch(getProductsSubCategorias('Salon', 'Bebidas', defaultSelectedOption));
-                }
+                dispatch(getProductsSubCategorias('Salon', 'Bebidas', defaultSelectedOption));
             }
         }
-    }, [defaultSelectedMenu, defaultSelectedOption, dispatch, cache]);
+    }, [defaultSelectedMenu, defaultSelectedOption, dispatch]);
 
     const handleBackClick = () => {
         navigate('/home');
@@ -53,52 +58,37 @@ const NavBar = ({ defaultSelectedMenu, defaultSelectedOption }) => {
         if (menu === 'Platos Tipicos') {
             setSelectedPlatoOption('Platos Tipicos');
             setSelectedBebidaOption('');
-            const subcategory = 'Platos Tipicos';
-            const cacheKey = `Salon_Platos Tipicos_${subcategory}`;
-            if (!cache[cacheKey]) {
-                dispatch(getProductsSubCategorias('Salon', menu, subcategory));
-            }
+            let subcategory = 'Platos Tipicos';
+            dispatch(getProductsSubCategorias('Salon', menu, subcategory));
         } else if (menu === 'Bebidas') {
             setSelectedBebidaOption('Bebidas sin alcohol');
             setSelectedPlatoOption('');
-            const subcategory = 'Bebidas sin alcohol';
-            const cacheKey = `Salon_Bebidas_${subcategory}`;
-            if (!cache[cacheKey]) {
-                dispatch(getProductsSubCategorias('Salon', menu, subcategory));
-            }
+            let subcategory = 'Bebidas sin alcohol';
+            dispatch(getProductsSubCategorias('Salon', menu, subcategory));
         }
     };
 
     const handleMenuClick = (menu) => {
         setSelectedMenu(menu);
-        const subcategory = menu === 'Platos Tipicos' ? 'Platos Tipicos' : 'Bebidas sin alcohol';
-        const cacheKey = `Salon_${menu}_${subcategory}`;
-        if (!cache[cacheKey]) {
-            dispatch(getProductsSubCategorias('Salon', menu, subcategory));
-        }
+        let subcategory = 'Platos Tipicos';
+        dispatch(getProductsSubCategorias('Salon', menu, subcategory));
     };
 
     const handleOptionClick = (option) => {
         setSelectedOption(option);
         dispatch(getProductsSubCategorias('Salon', selectedMenu, option));
     };
-
+//HANDLE PARA PLATOS O BEBIDAS 1° MENU
     const handlePlatoOptionClick = (option) => {
         setSelectedPlatoOption(option);
         setSelectedOption(option);
-        const cacheKey = `Salon_Platos Tipicos_${option}`;
-        if (!cache[cacheKey]) {
-            dispatch(getProductsSubCategorias('Salon', 'Platos Tipicos', option));
-        }
+        dispatch(getProductsSubCategorias('Salon', 'Platos Tipicos', option));
     };
-
+// HANDLE PARA BEBIDAS
     const handleBebidaOptionClick = (option) => {
         setSelectedBebidaOption(option);
         setSelectedOption(option);
-        const cacheKey = `Salon_Bebidas_${option}`;
-        if (!cache[cacheKey]) {
-            dispatch(getProductsSubCategorias('Salon', 'Bebidas', option));
-        }
+        dispatch(getProductsSubCategorias('Salon', 'Bebidas', option));
     };
 
     return (
@@ -123,174 +113,31 @@ const NavBar = ({ defaultSelectedMenu, defaultSelectedOption }) => {
                 >
                     Bebidas
                 </button>
-            </div>
-            <div className={style.menuOptions}>
-                {selectedMenu === 'Platos Tipicos' && platosOptions.map((option, index) => (
-                    <button
-                        key={index}
-                        className={`${style.optionButton} ${selectedPlatoOption === option ? style.active : ''}`}
-                        onClick={() => handlePlatoOptionClick(option)}
-                    >
-                        {option}
-                    </button>
-                ))}
 
-                {selectedMenu === 'Bebidas' && bebidasOptions.map((option, index) => (
-                    <button
-                        key={index}
-                        className={`${style.optionButton} ${selectedBebidaOption === option ? style.active : ''}`}
-                        onClick={() => handleBebidaOptionClick(option)}
-                    >
-                        {option}
-                    </button>
-                ))}
-            </div>
+                    </div>
+                <div className={style.menuOptions}>
+                    {selectedMenu === 'Platos Tipicos' && platosOptions.map((option, index) => (
+                        <button
+                            key={index}
+                            className={`${style.optionButton} ${selectedPlatoOption === option ? style.active : ''}`}
+                            onClick={() => handlePlatoOptionClick(option)}
+                        >
+                            {option}
+                        </button>
+                    ))}
+
+                    {selectedMenu === 'Bebidas' && bebidasOptions.map((option, index) => (
+                        <button
+                            key={index}
+                            className={`${style.optionButton} ${selectedBebidaOption === option ? style.active : ''}`}
+                            onClick={() => handleBebidaOptionClick(option)}
+                        >
+                            {option}
+                        </button>
+                    ))}
+                </div>
         </div>
     );
 };
 
 export default NavBar;
-
-// import React, { useState, useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
-// import style from './NavBar.module.css';
-// import { getProductsSubCategorias } from "../../redux/actions";
-
-// const NavBar = ({ defaultSelectedMenu, defaultSelectedOption }) => {
-//     const dispatch = useDispatch();
-//     const navigate = useNavigate();
-
-//     const [selectedMenu, setSelectedMenu] = useState(defaultSelectedMenu);
-//     const [selectedOption, setSelectedOption] = useState(defaultSelectedOption);
-//     const [selectedPlatoOption, setSelectedPlatoOption] = useState('');
-//     const [selectedBebidaOption, setSelectedBebidaOption] = useState('');
-
-//     // useEffect(() => {
-//     //     if (defaultSelectedMenu === 'Platos Tipicos') {
-//     //         setSelectedMenu('Platos Tipicos');
-//     //         setSelectedPlatoOption(defaultSelectedOption);
-//     //         setSelectedBebidaOption('');
-//     //         dispatch(getProductsSubCategorias('Salon', 'Platos Tipicos', defaultSelectedOption));
-//     //     } else if (defaultSelectedMenu === 'Bebidas') {
-//     //         setSelectedMenu('Bebidas');
-//     //         setSelectedPlatoOption('');
-//     //         setSelectedBebidaOption(defaultSelectedOption);
-//     //         dispatch(getProductsSubCategorias('Salon', 'Bebidas', defaultSelectedOption));
-//     //     }
-//     // }, [defaultSelectedMenu, defaultSelectedOption, dispatch]);
-//     useEffect(() => {
-//         if (defaultSelectedMenu && defaultSelectedOption) {
-//             if (defaultSelectedMenu === 'Platos Tipicos') {
-//                 setSelectedMenu('Platos Tipicos');
-//                 setSelectedPlatoOption(defaultSelectedOption);
-//                 setSelectedBebidaOption('');
-//                 dispatch(getProductsSubCategorias('Salon', 'Platos Tipicos', defaultSelectedOption));
-//             } else if (defaultSelectedMenu === 'Bebidas') {
-//                 setSelectedMenu('Bebidas');
-//                 setSelectedPlatoOption('');
-//                 setSelectedBebidaOption(defaultSelectedOption);
-//                 dispatch(getProductsSubCategorias('Salon', 'Bebidas', defaultSelectedOption));
-//             }
-//         }
-//     }, [defaultSelectedMenu, defaultSelectedOption, dispatch]);
-
-//     const handleBackClick = () => {
-//         navigate('/home');
-//     };
-
-//     const handleBackMenuClick = () => {
-//         navigate('/'); 
-//     };
-
-//     const platosOptions = ['Platos Tipicos', 'Postres Tradicionales'];
-//     const bebidasOptions = ['Bebidas sin alcohol', 'Bebidas con alcohol', 'Tragos tipicos', 'Cafeteria'];
-
-//     const handleMenuChange = (menu) => {
-//         setSelectedMenu(menu);
-//         if (menu === 'Platos Tipicos') {
-//             setSelectedPlatoOption('Platos Tipicos');
-//             setSelectedBebidaOption('');
-//             let subcategory = 'Platos Tipicos';
-//             dispatch(getProductsSubCategorias('Salon', menu, subcategory));
-//         } else if (menu === 'Bebidas') {
-//             setSelectedBebidaOption('Bebidas sin alcohol');
-//             setSelectedPlatoOption('');
-//             let subcategory = 'Bebidas sin alcohol';
-//             dispatch(getProductsSubCategorias('Salon', menu, subcategory));
-//         }
-//     };
-
-//     const handleMenuClick = (menu) => {
-//         setSelectedMenu(menu);
-//         let subcategory = 'Platos Tipicos';
-//         dispatch(getProductsSubCategorias('Salon', menu, subcategory));
-//     };
-
-//     const handleOptionClick = (option) => {
-//         setSelectedOption(option);
-//         dispatch(getProductsSubCategorias('Salon', selectedMenu, option));
-//     };
-// //HANDLE PARA PLATOS O BEBIDAS 1° MENU
-//     const handlePlatoOptionClick = (option) => {
-//         setSelectedPlatoOption(option);
-//         setSelectedOption(option);
-//         dispatch(getProductsSubCategorias('Salon', 'Platos Tipicos', option));
-//     };
-// // HANDLE PARA BEBIDAS
-//     const handleBebidaOptionClick = (option) => {
-//         setSelectedBebidaOption(option);
-//         setSelectedOption(option);
-//         dispatch(getProductsSubCategorias('Salon', 'Bebidas', option));
-//     };
-
-//     return (
-//         <div className={style.navBarContainer}>
-//             <div className={style.navbar}>
-//                 <div onClick={handleBackMenuClick} className={style.imageContainer}>
-//                     <img src="https://res.cloudinary.com/dmqszpq9k/image/upload/v1725428808/banderaInicio2_drufr0.jpg" alt="Bandera de Polonia" className={style.logo} />
-//                 </div>
-//                 <div onClick={handleBackClick} className={style.buttonBack}>
-//                     <img src="https://res.cloudinary.com/dmqszpq9k/image/upload/v1725438888/ATRAS2_p6ic2j.png" alt="regresar" className={style.logoBack} />
-//                 </div>
-
-//                 <button
-//                     className={`${style.navButton} ${selectedMenu === "Platos Tipicos" ? style.active : ''}`}
-//                     onClick={() => { handleMenuClick("Platos Tipicos"); handleMenuChange('Platos Tipicos'); }}
-//                 >
-//                     Platos Tipicos
-//                 </button>
-//                 <button
-//                     className={`${style.navButton} ${selectedMenu === "Bebidas" ? style.active : ''}`}
-//                     onClick={() => { handleMenuClick("Bebidas"); handleMenuChange('Bebidas'); }}
-//                 >
-//                     Bebidas
-//                 </button>
-
-//                     </div>
-//                 <div className={style.menuOptions}>
-//                     {selectedMenu === 'Platos Tipicos' && platosOptions.map((option, index) => (
-//                         <button
-//                             key={index}
-//                             className={`${style.optionButton} ${selectedPlatoOption === option ? style.active : ''}`}
-//                             onClick={() => handlePlatoOptionClick(option)}
-//                         >
-//                             {option}
-//                         </button>
-//                     ))}
-
-//                     {selectedMenu === 'Bebidas' && bebidasOptions.map((option, index) => (
-//                         <button
-//                             key={index}
-//                             className={`${style.optionButton} ${selectedBebidaOption === option ? style.active : ''}`}
-//                             onClick={() => handleBebidaOptionClick(option)}
-//                         >
-//                             {option}
-//                         </button>
-//                     ))}
-//                 </div>
-//         </div>
-//     );
-// };
-
-// export default NavBar;
